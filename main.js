5f06d1ddbf4d04c091f8ab5e989b83a5ec42b6bf
ctrachte <cetrachte@hotmail.com>
2018-06-04 12:01:35 -0500

    call new greetingss

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const greetings = require("./greeting.js");
+
 // "Hello"
 greetings.greetingEnglish();
 
 // "Konichiwa"
 greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
