91bbf61ab99c25796ffcd55d292b04cc69f8261d
ctrachte <cetrachte@hotmail.com>
2018-06-04 13:00:41 -0500

    add readline interface

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const greetings = require("./greeting.js");
 
-// "Hello"
-greetings.greetingEnglish();
+const readline = require('readline');
 
-// "Konichiwa"
-greetings.greetingJapanese();
 
-// "Hola"
-greetings.greetingSpanish();
 
-// "Hallo"
-greetings.greetingIcelandic();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Choose a language: English, Spanish, Japanese, Icelandic: ', (answer) => {
+
+	if (answer.toLowerCase() === 'spanish') {
+		greetings.greetingSpanish();
+	} else if (answer.toLowerCase() === 'icelandic') {
+		greetings.greetingIcelandic();
+	} else if (answer.toLowerCase() === 'japanese') {
+		greetings.greetingJapanese();
+	} else if (answer.toLowerCase() === 'english') {
+		greetings.greetingEnglish();
+	} else {
+		greetings.greetingEnglish();
+	}
+	 //close the input stream.
+	rl.close();
+});
